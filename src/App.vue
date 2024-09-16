20fe771fbc807ddb0b2ca6649b877172da8b7027
Karen Morado <khinojo7@stu.southtexascollege.edu>
2024-09-15 20:25:40 -0500

    Fixing the display of the recipe

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,35 @@
 <script setup>
-import {ref} from 'vue'
+import {ref, onMounted, watch} from 'vue'
+import {jsx} from 'vue/jsx-dev-runtime'
 
 const name = ref('')
+const input_ingr = ref('')
+const input_steps = ref('')
 
+const myIngr = ref([])
+const mySteps = ref([])
+
+
+//function to add ingredients
 const addIngredient = () =>{
   if(input_ingr.value.trim() === '' ){
     return
   }
-
+  myIngr.value.push({
+    content: input_ingr.value
+  })
+  input_ingr.value = ''
+  
 }
-
+const addStep = () => {
+  if (input_steps.value.trim() === '') {
+    return
+  }
+  mySteps.value.push({
+    content: input_steps.value
+  })
+  input_steps.value = ''
+}
 </script>
 
 <template>
@@ -29,6 +49,7 @@ const addIngredient = () =>{
     <form @submit.prevent = "addIngredient" >
       <h4>Insert your ingredients</h4>
       <input type="text" placeholder="e.g., 2 Eggs, tomatos, lettuce" v-model="input_ingr"/>
+      <input  type="submit" value="Add Ingredient" @click.prevent="addIngredient"/>
     </form>
 
   </section>
@@ -38,14 +59,30 @@ const addIngredient = () =>{
     <form @submit.prevent = "addStep" >
       <h4>Write the steps for the perfect dish</h4>
       <input type="text" placeholder="e.g., Mix the eggs in a bowl" v-model="input_steps"/>
+      <input type="submit" value="Add Step" @click.prevent="addStep"/>
     </form>
-
   </section>
 
+  <section class="Recipe">
+  <h3>Recipe</h3>
+  <h2>ingredients</h2>
+    <ul>
+      <li v-for="(ing, index) in myIngr" :key="index">  
+      {{ingr.content }}
+      </li>
+    </ul>
+
+    <h2>Steps</h2>
+    <ul>
+      <li v-for="(step, index) in mySteps" :key="index">  
+      {{ingr.content }}
+      </li>
+    </ul>
+    
+  </section>
   </main>
-</template>
+  </template>
+  
 
-<style scoped>
 
 
-</style>
